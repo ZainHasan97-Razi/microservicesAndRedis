46ebd45872d246a8769b6cba20870141f8534756
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2022-02-18 12:02:09 +0500

    Microservices are connected

--- service2 - configuring microservice/src/math/math.controller.ts
+++ service2 - configuring microservice/src/math/math.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices'
 
 @Controller('math')
 export class MathController {
     private logger = new Logger('MathController');
     constructor(private mathService: MathService){}
 
-    @Post('add')
-    async accumulate(@Body('data') data: number[]) {
+    // Define the message pattern for this method
+    // @Post('add')
+    @MessagePattern('add')
+    async accumulate(data: number[]) {
         console.log('dataa ==>>>>>> ', data);
         
         // this.logger.log('Adding ' + data.toString()); // log something on any call

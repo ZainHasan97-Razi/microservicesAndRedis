e91b6866b10f73514449847c5a8f8cb0298c4d55
Zain Hasan <54819492+ZainHasan97-Razi@users.noreply.github.com>
2022-02-21 10:57:55 +0500

    chnages

--- TCP/service3 - configuring client for microservice/src/math/math.service.ts
+++ TCP/service3 - configuring client for microservice/src/math/math.service.ts
@@ -25,24 +25,3 @@ export class MathService {
     }
 }
 
-// import {ClientProxyFactory, Transport, ClientOptions} from '@nestjs/microservices'
-// import {Logger} from '@nestjs/common'
-
-// const logger = new Logger('Main')
-
-// const microservicesOptions: ClientOptions = {
-//     transport: Transport.TCP,
-//     options: {
-//         host: '127.0.0.1',
-//         port: 8877
-//     },
-// }
-
-// const Client = ClientProxyFactory.create(microservicesOptions);
-
-// Client
-// // .send<ReturnType, ParamType>(pattern, param)
-// .send<number| number[]>('add', [1, 2, 3, 4, 5])
-// .subscribe(result => {
-//     logger.log('result => ', result)
-// })
